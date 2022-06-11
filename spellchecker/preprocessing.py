4b8cf189bf5b5ccb3ec4eefed4d1a0cc0842b22a
emrecncelik <emrecncelik@gmail.com>
2022-06-11 23:38:00 +0300

    lowercase uppercase for tr

--- spellchecker/preprocessing.py
+++ spellchecker/preprocessing.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 import re
 import string
+import logging
 from toolz import functoolz
 from nltk.tokenize import word_tokenize
 from joblib import Parallel, delayed
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
 
 
 class Preprocessor:
@@ -34,8 +38,9 @@ class Preprocessor:
         }
 
     def __call__(self, texts: list[str]) -> list[list[str]]:
+        logger.info("Preprocessing...")
         return Parallel(n_jobs=self.n_jobs, backend="multiprocessing")(
-            delayed(self.pipeline)(t) for t in texts
+            delayed(self.pipeline)(t) for t in tqdm(texts)
         )
 
     @property
@@ -77,10 +82,7 @@ class Preprocessor:
 
     @staticmethod
     def lowercase(text: str) -> str:
-        text = re.sub(r"İ", "i", text)
-        text = re.sub(r"I", "ı", text)
-        text = text.lower()
-        return text
+        return turkish_lower(text)
 
     @staticmethod
     def normalize_i(text: str) -> str:
@@ -95,6 +97,19 @@ def is_not_number(token: str, nums: set[str], puncts: set[str]) -> bool:
     return not all([ch in puncts or ch in nums for ch in token]) and is_not_punctuation(
         token, puncts
     )
+
+
+def turkish_lower(text: str) -> str:
+    text = re.sub(r"İ", "i", text)
+    text = re.sub(r"I", "ı", text)
+    text = text.lower()
+    return text
+
+
+def turkish_upper(text: str) -> str:
+    text = re.sub(r"i", "İ", text)
+    text = text.upper()
+    return text
 
 
 if __name__ == "__main__":
